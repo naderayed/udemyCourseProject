81d22ff4c6c77a0b03f7b9974c788112399b73fb
NaderAyed <nader.ayed@esprit.tn>
2023-02-19 00:05:15 +0100

    understanding how angular works

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['server.component.css']
+})
+export class ServerComponent {
+}
