86ccdb7d1ee6f3dbe0b5b55136e575e683f04bd9
NaderAyed <nader.ayed@esprit.tn>
2023-02-28 20:34:43 +0100

    dataBinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,11 @@ import { Component } from "@angular/core";
   styleUrls: ['server.component.css']
 })
 export class ServerComponent {
+  serverId :number =10;
+  serverStatus:string="offline";
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   // selector:'.app-servers',
   selector:'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styles:[
+    `h1{
+      color:red;
+    }`
+  ]
 })
 export class ServersComponent implements OnInit {
+  allowNewServer :boolean = false;
 
-  constructor() { }
+  constructor() {
+
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    },2000)
+  }
 
   ngOnInit(): void {
   }
